f64266483be9b3ed0608305ab69915428ec7db64
PC1 <leesx-wa15@student.tarc.edu.my>
2018-07-03 14:23:04 +0800

    added payment

--- app/src/main/java/my/edu/tarc/foodorderingapp/Payment.java
+++ app/src/main/java/my/edu/tarc/foodorderingapp/Payment.java
@@ -10,5 +10,6 @@ public class Payment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         //hello
+        //hello
     }
 }
